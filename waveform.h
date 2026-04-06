31a8c8406f8addf8e784d9168b1cc0326dd0edbf
ranabhullar <rana2.bhullar@live.uwe.uk.ac>
2026-04-06 17:50:08 +0100

    added waveform.h with waveformSample struct for csv data

---
+++ waveform.h
@@ -0,0 +1,15 @@
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
+
+typedef struct {
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+} WaveformSample;
+
+#endif
