645b112a4ff7712af0574c40583a396f3d8fa712
ranabhullar <rana2.bhullar@live.uwe.uk.ac>
2026-04-07 12:28:30 +0100

    added compute_peak_to_peak function to find max and min voltage per phase

--- waveform.c
+++ waveform.c
@@ -11,4 +11,20 @@ double compute_rms(WaveformSample *samples, int count, int phase) {
         sum_sq += voltage*voltage;
     }
     return sqrt(sum_sq/count);
+
+}
+double compute_peak_to_peak(WaveformSample *samples, int count, int phase) {
+    double max = -9999999.0;
+    double min =  9999999.0;
+
+    for (int i = 0; i < count; i++) {
+        double voltage;
+        if (phase == 0) voltage = samples[i].phase_A_voltage;
+        else if (phase == 1) voltage = samples[i].phase_B_voltage;
+        else voltage = samples[i].phase_C_voltage;
+
+        if (voltage > max) max = voltage;
+        if (voltage < min) min = voltage;
+    }
+    return max - min;
 }
