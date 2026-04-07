91bde0bbedecf676d7c9e7d76127663f34fdbbfb
ranabhullar <rana2.bhullar@live.uwe.uk.ac>
2026-04-07 11:55:34 +0100

    updated main.c to accept filename as command line argument using argv

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "waveform.h"
+#include "io.h"
 
-WaveformSample *load_csv(const char *filename, int *count);
+int main(int argc, char *argv[]) {
 
-int main(void) {
+    if (argc < 2) {
+        printf("Usage: ./PowerQualityAnalyser power_quality_log.csv\n");
+        return 1;
+    }
 
     int count = 0;
-    WaveformSample *samples = load_csv("power_quality_log.csv", &count);
+    WaveformSample *samples = load_csv(argv[1], &count);
 
     if (samples == NULL) {
         return 1;
     }
 
-    printf("===>>>>> Power Quality Analyser <<<<<===\n");
+    printf("=== Power Quality Analyser ===\n\n");
     printf("Loaded %d samples\n\n", count);
+
+    // Print first 5 rows to verify
     for (int i = 0; i < 5; i++) {
         printf("Time: %.4f | PhaseA: %.2f | PhaseB: %.2f | PhaseC: %.2f\n",
                samples[i].timestamp,
