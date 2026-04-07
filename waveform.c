e57dbb7d880076d73132c4558559746044c1a642
ranabhullar <rana2.bhullar@live.uwe.uk.ac>
2026-04-07 12:07:42 +0100

    added a compute_rms function to calculate RMS voltage for all 3 phases

---
+++ waveform.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include "waveform.h"
+double compute_rms(WaveformSample *samples, int count, int phase) {
+    double sum_sq = 0.0;
+    for (int i = 0; i < count; i++) {
+        double voltage;
+        if (phase == 0) voltage = samples[i].phase_A_voltage;
+        else if (phase == 1) voltage = samples[i].phase_B_voltage;
+        else voltage = samples[i].phase_C_voltage;
+        sum_sq += voltage*voltage;
+    }
+    return sqrt(sum_sq/count);
+}
