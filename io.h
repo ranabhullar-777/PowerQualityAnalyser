8f8e76497f78dccf6f20c2d7153899617dc812e5
ranabhullar <rana2.bhullar@live.uwe.uk.ac>
2026-04-08 12:52:57 +0100

    added write_results function declaration to io.h

--- io.h
+++ io.h
@@ -2,4 +2,12 @@
 #define IO_H
 #include "waveform.h"
 WaveformSample *load_csv(const char *filename, int *count);
+void write_results(const char *filename, WaveformSample *samples, int count,
+                   double rms_a, double rms_b, double rms_c,
+                   double pp_a, double pp_b, double pp_c,
+                   double dc_a, double dc_b, double dc_c,
+                   int clipped_a, int clipped_b, int clipped_c,
+                   double freq_min, double freq_max,
+                   double pf_min, double pf_max,
+                   double thd_min, double thd_max);
 #endif
